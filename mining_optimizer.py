682d7d6b48b2c81c7ed71155301cefdad702c471
Unknown <reynolds.j.a.119@gmail.com>
2018-08-03 13:16:40 +0200

    Update 1

--- mining_optimizer.py
+++ mining_optimizer.py
@@ -51,7 +51,7 @@ def get_distances(locations):
 
     return distances
 
-def get_distance_from_start(distances, locations):
+def add_distance_from_start(distances, locations):
     for key, value in distances.items():
         mine_coord = locations[key]
         distance_from_start = distance([0,0], mine_coord)
@@ -59,51 +59,123 @@ def get_distance_from_start(distances, locations):
 
     return distances
 
+def get_distance_from_start(distances_dict, locations):
+    distances = {}
+
+    for key, loc in distances_dict.items():
+        mine_coord = locations[key]
+        distance_from_start = distance([0, 0], mine_coord)
+        distances[key] = distance_from_start
+
+    ordered = sorted(distances.items(), key=lambda x: x[1])
+    return ordered
+
 def get_isolation_bias(distances, locations):
     for key, value in distances.items():
         mine_coord = locations[key.lower()]
         for key, value in distances.items():
             distance_to_next_node = distance(mine_coord, locations[key])
-            isolation_bias = distance_to_next_node / len(distances)
+            isolation_bias = (distance_to_next_node / len(distances))
             distances[key] = distances[key] + isolation_bias
     return distances
+
+def get_distance_to_next_depot(distances, locations):
+    depot_dict = {}
+    mine_dict = {}
+    for key, value in distances.items():
+        if key.lower() not in depot_dict.keys():
+            depot_dict[key.lower()] = locations[key.lower()]
+
+        if key not in mine_dict.keys():
+            mine_dict[key] = locations[key]
+
+    for depot_key, depot_loc in depot_dict.items():
+        depot_dict[depot_key] = {}
+        for mine_key, mine_loc in mine_dict.items():
+            result = distance(depot_loc, mine_loc)
+
+            depot_dict[depot_key][mine_key] = result
+
+    for depot_key, mine_dict in depot_dict.items():
+        mine_ordered = order_distances(mine_dict)
+        depot_dict[depot_key] = mine_ordered
+
+    return depot_dict
+
+
+
 
 def order_distances(distances):
     ordered = sorted(distances.items(), key=lambda x: x[1])
     return ordered
 
 
-def allocate_workers(workers, work_list, distances):
+def allocate_workers(workers, work_list, distances, depot_disances):
     allocations = []
     non_valid_options = []
     current_worker = 0
 
+    print(work_list)
+    print(distances)
+
     for x in range(workers):
         allocations.append([])
-    print(work_list)
-    for item in work_list:
-        if item not in non_valid_options:
-            pprint(distances)
-            for key, value in distances.items():
-                if key is not item:
-                    result = distances[key] / distances [item[0]]
-                    if 0.95 <= result <= 1.05:
-                        # print("Cluster found: " + key )
-                        if item not in non_valid_options:
-                            allocations[current_worker].append(item)
-                            non_valid_options.append(item)
-            # allocations[current_worker].append(item)
-        current_worker = current_worker + 1 if current_worker < workers - 1 else 0
-            # non_valid_options.append(item)
 
-    workers_list = []
-    for index, worker_thread in enumerate(allocations):
-        workers_list.append([])
-        for index2, item in enumerate(worker_thread):
-            workers_list[index].append(item[0])
+        allocations[x].append(work_list[x])
 
 
-    return workers_list
+    # for item in work_list:
+    #     if item not in non_valid_options:
+    #         for key, value in distances.items():
+    #             if key is not item:
+    #                 result = distances[key] / distances [item[0]]
+    #                 if 0.95 <= result <= 1.05:
+    #                     # print("Cluster found: " + key )
+    #                     if item not in non_valid_options:
+    #                         allocations[current_worker].append(item)
+    #                         non_valid_options.append(item)
+    #         # allocations[current_worker].append(item)
+    #     current_worker = current_worker + 1 if current_worker < workers - 1 else 0
+    #         # non_valid_options.append(item)
+    #
+    # workers_list = []
+    # for index, worker_thread in enumerate(allocations):
+    #     workers_list.append([])
+    #     for index2, item in enumerate(worker_thread):
+    #         workers_list[index].append(item[0])
+    #
+    #
+    # return workers_list
+
+def new_allocations(workers, start_dists, travel_dists, depot_distances, locations):
+    allocations = []
+    non_valid_options = []
+    current_worker = 0
+    for x in range(workers):
+        allocations.append([])
+
+    for x in range(workers):
+        for allocation in allocations:
+            if len(allocation) <=0:
+                if start_dists[0] not in non_valid_options:
+                    allocation.append(start_dists[0])
+                    non_valid_options.append(start_dists[0])
+                last_allocation = allocation[-1]
+                check_dist = depot_distances[last_allocation[0].lower()][0]
+
+                for item in start_dists:
+                    dist_from_start = distance([0,0], locations[check_dist[0]])
+                    print(dist_from_start)
+
+                    if check_dist[1] < dist_from_start:
+                        print("going to nex tlocation is closer than dispatching new worker. continue")
+                        break
+
+
+
+
+
+    print(allocations)
 
 def print_allocations(allocations):
     string_list = []
@@ -124,15 +196,19 @@ def print_allocations(allocations):
         f.write(output)
     print(local_filename)
 
-filename = "map_3.input"
+filename = "map_1.input"
 workers, field_map = map_setup(filename)
 location_dict = get_locations(field_map)
-distance_dict = get_distances(location_dict)
-distance_dict = get_distance_from_start(distance_dict, location_dict)
-distance_dict = get_isolation_bias(distance_dict, location_dict)
-orders = order_distances(distance_dict)
-allocations = allocate_workers(workers, orders, distance_dict)
-print_allocations(allocations)
+travel_distance_dict = get_distances(location_dict)
+start_distance_dict = get_distance_from_start(travel_distance_dict, location_dict)
+depot_distances = get_distance_to_next_depot(travel_distance_dict,location_dict)
+new_allocations(workers, start_distance_dict, travel_distance_dict, depot_distances, location_dict)
+# distance_dict = add_distance_from_start(distance_dict, location_dict)
+# distance_dict = get_isolation_bias(distance_dict, location_dict)
+#
+# orders = order_distances(distance_dict)
+# allocations = allocate_workers(workers, orders, distance_dict, depot_distances)
+# print_allocations(allocations)
 
 tend = datetime.now()
 print("Ended: " + str((tend - tstart).total_seconds()))
